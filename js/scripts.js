e7ce6e7f45273f4786b8f6b64720041ca99080f6
Iza M <imalnowicz@gmail.com>
2017-03-06 15:07:20 +0100

    Triangle Area

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+var a = prompt ('Wpisz długość boku trójkąta'),
+    h = prompt ('Wpisz wysokość trójkąta'),
+    triangleArea = 0.5*a*h;
+alert ('Pole trójkąta ' + 0.5*a*h)
+console.log('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea);
