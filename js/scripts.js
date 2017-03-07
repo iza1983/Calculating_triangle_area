72800b3d081776192e7c2bddeeea9322fcdb7770
Iza M <imalnowicz@gmail.com>
2017-03-07 14:16:32 +0100

    Między operatorami i argumentami w działaniach arytmetycznych dosałam spację. Oddzieliłam pustą linią deklarację zmiennych od reszty kodu. Dodałam brakujący średnik za alertem.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
-var a = prompt ('Wpisz długość boku trójkąta'),
-    h = prompt ('Wpisz wysokość trójkąta'),
-    triangleArea = 0.5*a*h;
-alert ('Pole trójkąta ' + 0.5*a*h)
+var a = prompt('Wpisz długość boku trójkąta'),
+    h = prompt('Wpisz wysokość trójkąta'),
+    triangleArea = 0.5 * a * h;
+	
+alert('Pole trójkąta ' + 0.5 * a * h);
 console.log('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea);
